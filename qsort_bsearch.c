b4ad199f4cd5c2e2cb194aaa038e086ea0b30d09
Jay <117885838+jay0011@users.noreply.github.com>
2023-02-01 21:45:25 +0530

    Quick Sort and Binary Search -> Recursive Approach

--- qsort_bsearch.c
+++ qsort_bsearch.c
@@ -1,7 +1,6 @@
 #include<stdio.h>
 #define size 20
 
-
 void swap(int *a,int *b){
 	*b=*a+*b-(*a=*b);
 }
@@ -24,18 +23,48 @@ void quick_sort(int arr[],int left,int right){
 	quick_sort(arr,p+1,right);
 }
 
+
+void binary_search(int arr[],int left,int right,int key){
+	int mid=left+(right-left)/2;
+	//Base Case:
+	if(left>right){
+		printf("The searching value not found in given array.\n");
+		return;
+	}
+	if(key==arr[mid]){
+		printf("The searching value found at %d position.\n",mid+1);
+		return;
+	}
+	//Recursive Case:
+	if(key<arr[mid]) binary_search(arr,left,mid-1,key);
+	else if(key>arr[mid]) binary_search(arr,mid+1,right,key);
+}
+
+
 int main(){
+	int i,n,key;
 	int arr[size];
-	int i,n;
+
 	printf("Enter the size of array = ");
 	scanf("%d",&n);
 	printf("Enter the %d element of array:\n",n);
 	for(i=0;i<n;i++)
 		scanf("%d",&arr[i]);
+
+	printf("Enter the searching value = ");
+	scanf("%d",&key);
+
 	quick_sort(arr,0,n-1);
+
 	printf("\nAfter the sorting\n");
 	for(i=0;i<n;i++)
 		printf("%d ",arr[i]);
+	printf("\n");
+
+	binary_search(arr,0,n-1,key);
+
+
+
 }
 
 //code by jay-neo
